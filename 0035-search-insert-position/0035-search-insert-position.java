77b2c15e49253f04dd323251bc6204dda05490e8
yeahjinjeong <88896562+yeahjinjeong@users.noreply.github.com>
2024-06-07 13:24:53 +0900

    Time: 1 ms (100%), Space: 42.6 MB (83.97%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (target == nums[i]) {
+                return i;
+            }
+        }
+        int[] newNums = Arrays.copyOf(nums, nums.length+1);
+        newNums[nums.length] = target;
+        Arrays.sort(newNums);
+        for (int j = 0; j < newNums.length; j++) {
+            if (target == newNums[j]) {
+                return j;
+            }
+        }
+        return 0;
+    }
+}
