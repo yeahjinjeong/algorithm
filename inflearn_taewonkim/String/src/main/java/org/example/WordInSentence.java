afd136203bb4a3f8cfff320cfa7ee84730f667eb
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-10-10 19:47:03 +0900

    Feat: WordInSentence #1

--- inflearn_taewonkim/String/src/main/java/org/example/WordInSentence.java
+++ inflearn_taewonkim/String/src/main/java/org/example/WordInSentence.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
+/**
+ * 3. 문장 속 단어
+ */
 public class WordInSentence {
     public static void main(String[] args) {
+        WordInSentence wordInSentence = new WordInSentence();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
+        System.out.println(wordInSentence.solution(input));
+    }
+
+    public String solution(String str) {
+        String[] strings = str.split(" ");
+        int max = 0;
+        String result = "";
+        for (String s : strings) {
+            if (s.length() > max) {
+                max = s.length();
+                result = s;
+            }
+        }
+        return result;
     }
 }
