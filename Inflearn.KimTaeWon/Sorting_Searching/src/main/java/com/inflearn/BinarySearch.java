e5664902d8fabb5cb707bb07828b2b0d303903ce
yeahjinjeong <jjinnyzzang@gmail.com>
2024-11-15 16:18:40 +0900

    Feat: 이분 검색, 오름차순 정렬 후 mid를 초기화해주기 #6

---
+++ Inflearn.KimTaeWon/Sorting_Searching/src/main/java/com/inflearn/BinarySearch.java
@@ -0,0 +1,44 @@
+package com.inflearn;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        BinarySearch binarySearch = new BinarySearch();
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(binarySearch.solution(n, m, arr));
+
+        Arrays.sort(arr);
+        System.out.print(Arrays.binarySearch(arr, m) + 1);
+    }
+
+    private int solution(int n, int m, int[] arr) {
+        Arrays.sort(arr);
+
+        int lt = 0, rt = n-1;
+        int mid = 0;
+        while (lt <= rt) {
+            mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                return mid + 1;
+            } else if (arr[mid] < m) {
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return mid + 1;
+    }
+}
