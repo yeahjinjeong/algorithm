4ffd4b00505424c1e02909549c48e7152c196151
yeahjinjeong <jjinnyzzang@gmail.com>
2024-11-15 12:20:45 +0900

    Update: 장난꾸러기 강의 풀이 추가 #6

--- Inflearn.KimTaeWon/Sorting_Searching/src/main/java/com/inflearn/Naughty.java
+++ Inflearn.KimTaeWon/Sorting_Searching/src/main/java/com/inflearn/Naughty.java
@@ -1,5 +1,7 @@
 package com.inflearn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Naughty {
@@ -15,21 +17,46 @@ public class Naughty {
             arr[i] = sc.nextInt();
         }
 
-        String solution = naughty.solution(n, arr);
-        System.out.print(solution);
+        for (int i : naughty.solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+
+        for (int i : naughty.solution2(n, arr)) {
+            System.out.print(i + " ");
+        }
     }
 
-    private String solution(int n, int[] arr) {
+    private ArrayList<Integer> solution2(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+        for (int i = 0; i < n; i++) {
+            if (tmp[i] != arr[i]) {
+                answer.add(i + 1);
+            }
+        }
+        return answer;
+    }
+
+    private ArrayList<Integer> solution(int n, int[] arr) {
         int lt = 0, rt = n-1;
+
         int CHEOLSU = 0;
         int pair = 0;
-        while (CHEOLSU == 0 || pair == 0) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        while (CHEOLSU == 0 || pair == 0 && lt != rt) {
             if (arr[lt] > arr[lt+1]) CHEOLSU = lt + 1;
             if (arr[rt-1] > arr[rt]) pair = rt + 1;
 
             lt++;
             rt--;
         }
-        return CHEOLSU + " " + pair;
+
+        answer.add(CHEOLSU);
+        answer.add(pair);
+        return answer;
     }
+
+
 }
