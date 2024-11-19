5bdd926eafb1ceb3156aa4d85e3efd35a7c4d902
yeahjinjeong <jjinnyzzang@gmail.com>
2024-11-19 14:59:52 +0900

    Update: 마구간 정하기 주석 추가, 거리의 범위를 대강 잡고 >= 등호에 걸리기를 기대하기 #6

--- Inflearn.KimTaeWon/Sorting_Searching/src/main/java/com/inflearn/EstablishStall.java
+++ Inflearn.KimTaeWon/Sorting_Searching/src/main/java/com/inflearn/EstablishStall.java
@@ -37,28 +37,27 @@ public class EstablishStall {
         int answer = 0;
         Arrays.sort(arr);
 
-        int lt = 1;
-        int rt = arr[n - 1];
+        int lt = 1; // 거리는 최소 1
+        int rt = arr[n - 1]; // 최대 마지막 좌표값 => (마지막 - 첫번째)가 정확하지만, 한 번의 반복은 성능에 큰 영향을 끼치지 않음
         while (lt <= rt) {
             int mid = (lt + rt) / 2;
-            if (count(arr, mid) >= c) {
-                answer = mid;
-                lt = mid + 1;
-            } else rt = mid - 1;
+            if (count(arr, mid) >= c) { // 배치한 말이 주어진 말의 개수보다 크거나 같으면,  이 말은 최대는 아닐 수 있어도 일단 배치는 가능하다는 뜻
+                answer = mid; // 후보군이 될 수 있는 것!
+                lt = mid + 1; // 최대를 확인하기 위해 범위를 좁힌다
+            } else rt = mid - 1; // 배치한 말이 적으면 배치를 못 시킬 정도로 간격이 넓다는 뜻이므로 거리를 줄여야 함
         }
         System.out.println(answer);
     }
 
-    private int count(int[] arr, int dist) {
+    private int count(int[] arr, int distance) {
         int cnt = 1;
-        int ep = arr[0];
+        int ep = arr[0]; // 첫 말은 일단 맨 앞 마구간에 두는 게 최대 거리를 구하기 유리함! 두세 번째에 두면 그 거리가 낭비됨
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - ep >= dist) { // 두 말 사이의 거리가 주어진 값보다 크면
-                cnt++;
-                ep = arr[i];
+            if (arr[i] - ep >= distance) { // 두 말 사이의 거리가 주어진 값보다 바로 크거나 같으면
+                cnt++; // 배치를 시킴!
+                ep = arr[i]; // 그리고 새롭게 배치된 말이 ep가 됨!
             }
         }
         return cnt;
     }
-
 }
