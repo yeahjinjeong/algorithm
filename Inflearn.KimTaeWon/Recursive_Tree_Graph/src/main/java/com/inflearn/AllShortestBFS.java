830d877249458e083be196778f602c07e8d5f33e
yeahjinjeong <jjinnyzzang@gmail.com>
2024-12-03 15:40:16 +0900

    Refactor: 그래프 최단거리 - 방문 여부를 추가하여 최초 풀이를 디벨롭함 #7

--- Inflearn.KimTaeWon/Recursive_Tree_Graph/src/main/java/com/inflearn/AllShortestBFS.java
+++ Inflearn.KimTaeWon/Recursive_Tree_Graph/src/main/java/com/inflearn/AllShortestBFS.java
@@ -9,21 +9,28 @@ public class AllShortestBFS {
     static int n, m;
     static int distance = 0;
     static int[][] graph;
-    static int[] answer;
+    static int[] answer, visit;
 
     private void BFS() {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
         answer[0] = -1;
         answer[1] = -1;
-        while (Arrays.stream(answer).anyMatch(a -> a == 0)) { // answer 배열이 다 채워지는 순간 종료
+        while (!queue.isEmpty()) { // queue에 중복되지 않은 정점들만 들어올 것
             for (int s = 0; s < queue.size(); s++) { // 한 레벨씩 돌면서 필터링 하는 느낌
                 int cv = queue.poll();
-                for (int j = 1; j <= n; j++) {
-                    if (graph[cv][j] == 1) {
-                        if (answer[j] == 0) answer[j] = distance + 1; // 제일 먼저 나오는대로 초기화하기
-                        queue.offer(j); // 가리킴 당하는 정점이 큐에 추가됨 -> 그러나 가리키는 것이 없다면 삭제됨
+                for (int nv = 1; nv <= n; nv++) {
+                    if (graph[cv][nv] == 1 && visit[nv] == 0) { // 방문 여부 추가
+                        visit[nv] = 1; // 방문한 적 있으면 이미 큐에 추가되어서 최단 거리로 나올 수 있는 경우의 수는 다 나옴
+                        if (answer[nv] == 0) answer[nv] = distance + 1; // 제일 먼저 나오는대로 초기화하기
+                        queue.offer(nv); // 가리킴 당하는 정점이 큐에 추가됨 -> 그러나 가리키는 것이 없다면 삭제됨
                     }
+
+//                    if (graph[cv][nv] == 1) { // 방문 여부 X = 중복 여부 X
+//                        if (answer[nv] == 0) answer[nv] = distance + 1; // 제일 먼저 나오는대로 초기화하기
+//                        queue.offer(nv); // 가리킴 당하는 정점이 큐에 추가됨 -> 그러나 가리키는 것이 없다면 삭제됨
+//                    } => 이 방식을 쓰려면 while (Arrays.stream(answer).anyMatch(a -> a == 0)) 이렇게 조건문을 써주어야 함
+
                 }
             }
             distance++;
@@ -31,7 +38,7 @@ public class AllShortestBFS {
     }
 
     static ArrayList<ArrayList<Integer>> graph2;
-    static int[] answer2, visit;
+    static int[] answer2, visit2;
 
     private void BFS2() {
         Queue<Integer> queue = new LinkedList<>();
@@ -41,10 +48,10 @@ public class AllShortestBFS {
             for (int i = 0; i < queue.size(); i++) {
                 int cv = queue.poll();
                 for (int nv : graph2.get(cv)) {
-                    if (visit[nv] == 0) {
-                        visit[nv] = 1;
-                        queue.offer(nv);
+                    if (visit2[nv] == 0) {
+                        visit2[nv] = 1; // 방문한 적 있으면 방문했을 때 이미 최단거리가 결정났겠으므로
                         answer2[nv] = answer2[cv] + 1;
+                        queue.offer(nv);
                     }
                 }
             }
@@ -68,6 +75,7 @@ public class AllShortestBFS {
         answer2 = new int[n + 1];
 
         visit = new int[n + 1];
+        visit2 = new int[n + 1];
 
         m = sc.nextInt();
 
