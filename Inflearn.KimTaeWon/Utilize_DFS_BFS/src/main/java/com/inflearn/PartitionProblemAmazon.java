331d0601ae18d333f21bf65445b21be61dce5a68
yeahjinjeong <jjinnyzzang@gmail.com>
2024-12-03 17:38:18 +0900

    Refactor: 합이 같은 부분집합 강의 풀이 참고 및 수정 (sum 인자와 flag사용) #8

--- Inflearn.KimTaeWon/Utilize_DFS_BFS/src/main/java/com/inflearn/PartitionProblemAmazon.java
+++ Inflearn.KimTaeWon/Utilize_DFS_BFS/src/main/java/com/inflearn/PartitionProblemAmazon.java
@@ -5,32 +5,12 @@ import java.util.*;
 public class PartitionProblemAmazon {
 
     static Map<Integer, Integer> arr; // key에 원소, value에 집합 포함 여부
-    static List<Boolean> answer; // sum 저장
     static int n; // 원소의 개수
+    static boolean flag = false;
 
-    public static void main(String[] args) {
-        PartitionProblemAmazon partitionProblemAmazon = new PartitionProblemAmazon();
-
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-
-        arr = new HashMap<>();
-        answer = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            arr.put(a, 0);
-        }
-
-        partitionProblemAmazon.DFS(0);
-
-        if (answer.contains(true)) {
-            System.out.println("YES");
-        } else System.out.println("NO");
-    }
-
-    private void DFS(int i) {
-        if (i == n) { // arr을 0부터 도는 중 ( 0부터 시작했으므로 n, 만약 1부터 시작했다면 n+1)
+    private void DFS(int L) {
+        if (flag) return;
+        if (L == n) { // arr을 0부터 도는 중 ( 0부터 시작했으므로 n, 만약 1부터 시작했다면 n+1)
             int sum1 = 0, sum2 = 0;
             for (int k : arr.keySet()) {
                 if (arr.get(k) == 1) {
@@ -40,14 +20,62 @@ public class PartitionProblemAmazon {
                 }
             }
             if (sum1 == sum2) {
-                answer.add(true);
+                flag = true;
             }
         } else {
-            int key = (int) arr.keySet().toArray()[i];
+            int key = (int) arr.keySet().toArray()[L];
             arr.put(key, 1);
-            DFS(i + 1); // 인덱스 올려서 호출
+            DFS(L + 1); // 인덱스 올려서 호출
             arr.put(key, 0);
-            DFS(i + 1);
+            DFS(L + 1);
         }
     }
+
+    static boolean flag2 = false;
+    static int[] arr2;
+    static int total = 0;
+
+    private void DFS(int L, int sum) {
+        if (flag2) return;
+        if (sum > total/2) return;
+        if (L == n) {
+            if ((total - sum) == sum) {
+                flag2 = true;
+            }
+        } else {
+            DFS(L + 1, sum+arr2[L]); // 부분집합에 속함
+            DFS(L + 1, sum); // 부분집합에 속하지 않음
+        }
+    }
+
+    public static void main(String[] args) {
+        PartitionProblemAmazon partitionProblemAmazon = new PartitionProblemAmazon();
+
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        arr = new HashMap<>();
+        arr2 = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            arr.put(a, 0);
+            arr2[i] = a;
+            total += a;
+        }
+
+        partitionProblemAmazon.DFS(0);
+        if (flag) {
+            System.out.println("YES");
+        }
+
+        partitionProblemAmazon.DFS(0, 0);
+        if (flag2) {
+            System.out.println("YES");
+        } else System.out.println("NO");
+    }
 }
+/*
+6
+1 3 5 6 7 10
+ */
