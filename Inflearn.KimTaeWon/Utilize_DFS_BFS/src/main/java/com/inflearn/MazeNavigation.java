8be92c2f8538d381ce3f228f054efa129d483be5
yeahjinjeong <jjinnyzzang@gmail.com>
2024-12-08 20:31:26 +0900

    Update: 미로 탐색 2nd try #8

--- Inflearn.KimTaeWon/Utilize_DFS_BFS/src/main/java/com/inflearn/MazeNavigation.java
+++ Inflearn.KimTaeWon/Utilize_DFS_BFS/src/main/java/com/inflearn/MazeNavigation.java
@@ -6,17 +6,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MazeNavigation {
-    static int[][] board, visit;
+    static int[][] board;
+    // 상 하 좌 우
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
     static int answer = 0;
-    static List<int[][]> ca;
     public static void main(String[] args) {
         MazeNavigation mazeNavigation = new MazeNavigation();
 
         Scanner sc = new Scanner(System.in);
 
         board = new int[8][8];
-        visit = new int[8][8];
-        ca = new ArrayList<>();
 
         for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= 7; j++) {
@@ -24,23 +24,24 @@ public class MazeNavigation {
             }
         }
 
+        board[1][1] = 1;
         mazeNavigation.DFS(1, 1);
         System.out.println(answer);
 
     }
 
     private void DFS(int x, int y) {
-        if (x == 0 || y == 0 || x == 8 || y == 8) return;
-        if (x == 7 && y == 7) {
-            answer++;
-        }
-        if (board[x][y] == 0 && visit[x][y] == 0) {
-            visit[x][y] = 1;
-            DFS(x - 1, y);
-            DFS(x + 1, y);
-            DFS(x, y - 1);
-            DFS(x, y + 1);
-            visit[x][y] = 0;
+        if (x == 7 && y == 7) answer++;
+        else {
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx > 0 && ny > 0 && nx < 8 && ny < 8 && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    DFS(nx, ny);
+                    board[nx][ny] = 0;
+                }
+            }
         }
     }
 }
