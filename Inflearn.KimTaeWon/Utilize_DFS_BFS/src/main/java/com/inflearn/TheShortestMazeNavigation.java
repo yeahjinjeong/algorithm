e5a75c286bfc2439cf25e25592668126954a82de
yeahjinjeong <jjinnyzzang@gmail.com>
2024-12-11 20:09:23 +0900

    Chore: 미로 탐색 좌표 클래스명 수정 #8

--- Inflearn.KimTaeWon/Utilize_DFS_BFS/src/main/java/com/inflearn/TheShortestMazeNavigation.java
+++ Inflearn.KimTaeWon/Utilize_DFS_BFS/src/main/java/com/inflearn/TheShortestMazeNavigation.java
@@ -20,11 +20,11 @@ public class TheShortestMazeNavigation {
             }
         }
 
-        System.out.println(theShortestMazeNavigation.BFS(new Site(1, 1), new Site(7, 7)));
+        System.out.println(theShortestMazeNavigation.BFS(new Point(1, 1), new Point(7, 7)));
     }
 
-    private int BFS(Site start, Site end) {
-        Queue<Site> queue = new LinkedList<>();
+    private int BFS(Point start, Point end) {
+        Queue<Point> queue = new LinkedList<>();
         int distance = 0;
 
         queue.offer(start);
@@ -32,9 +32,9 @@ public class TheShortestMazeNavigation {
         while (!queue.isEmpty()) {
             int qSize = queue.size();
             for (int q = 0; q < qSize; q++) { // 병렬적으로 계산함
-                Site cur = queue.poll();
+                Point cur = queue.poll();
                 for (int i = 0; i < 4; i++) {
-                    Site next = new Site(cur.x + dx[i], cur.y + dy[i]);
+                    Point next = new Point(cur.x + dx[i], cur.y + dy[i]);
                     if (next.x == end.x && next.y == end.y) return distance + 1;
                     if (next.x >= 1 && next.x <= 7 && next.y >= 1 && next.y <= 7 && board[next.x][next.y] == 0) {
                         board[next.x][next.y] =  1;
@@ -46,23 +46,5 @@ public class TheShortestMazeNavigation {
         }
 
         return -1;
-    }
-}
-
-class Site {
-    int x;
-    int y;
-
-    public Site(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 }
