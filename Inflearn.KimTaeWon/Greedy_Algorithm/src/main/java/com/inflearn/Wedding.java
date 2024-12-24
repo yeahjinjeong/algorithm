c0e5dde304b5ef2ac50a1bbf2c2c31a8ba3f4421
yeahjinjeong <jjinnyzzang@gmail.com>
2024-12-24 09:13:50 +0900

    Update: 결혼식- 그리디 알고리즘(시간객체 사용하기) #10

--- Inflearn.KimTaeWon/Greedy_Algorithm/src/main/java/com/inflearn/Wedding.java
+++ Inflearn.KimTaeWon/Greedy_Algorithm/src/main/java/com/inflearn/Wedding.java
@@ -10,44 +10,58 @@ public class Wedding {
 
         int n = sc.nextInt();
 
-        List<Attender> attenders = new ArrayList<>();
+        List<Time> times = new ArrayList<>();
         Map<Integer, Integer> table = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             int come  = sc.nextInt();
             int leave = sc.nextInt();
-//            attenders.add(new Attender(come, leave));
-            for (int j = come; j < leave; j++) {
-                table.put(j, table.getOrDefault(j, 0) + 1);
-            }
+            times.add(new Time(come, 'c'));
+            times.add(new Time(leave, 'l'));
+//            for (int j = come; j < leave; j++) {
+//                table.put(j, table.getOrDefault(j, 0) + 1);
+//            }
         }
 
-//        Collections.sort(attenders);
-//        wedding.solution(attenders);
-        int answer = 0;
-        for (int k : table.keySet()) {
-            answer = Math.max(answer, table.get(k));
-        }
+        System.out.println(wedding.solution(times));
 
-        System.out.println(answer);
+//        int answer = 0;
+//        for (int k : table.keySet()) {
+//            answer = Math.max(answer, table.get(k));
+//        }
+//        System.out.println(answer);
     }
 
-    private void solution(List<Attender> attenders) {
+    private int solution(List<Time> times) {
+        Collections.sort(times);
+
+        int cnt = 0;
+        int answer = Integer.MIN_VALUE;
+
+        for (Time a : times) {
+            if (a.state == 'c') cnt++;
+            else cnt--;
+            answer = Math.max(answer, cnt);
+        }
+        return answer;
     }
 }
 
-class Attender implements Comparable<Attender> {
-    int come;
-    int leave;
+class Time implements Comparable<Time> { // 사람 기준이 아닌, 시간 기준!
+    int time;
+    char state;
 
-    public Attender(int come, int leave) {
-        this.come = come;
-        this.leave = leave;
+    public Time(int time, char state) {
+        this.time = time;
+        this.state = state;
     }
 
     @Override
-    public int compareTo(Attender o) {
-        if (this.leave == o.leave) return this.come - o.come;
-        return this.leave - o.leave;
+    public int compareTo(Time o) {
+        if (this.time == o.time) return o.state - this.state; // l이 c보다 먼저오도록! 같은 14시인데 떠나는 사람과 온 사람이 있을 때
+        // 떠나는 사람이 먼저 온 사람이므로
+        // 먼저 온 사람 것을 먼저 처리해준다
+        // 만약 온 사람 것을 먼저 처리한다면, 이미 떠난 사람을 -(마이너스) 하기 전에 +(플러스) 먼저 계산돼서 over된다.
+        return this.time - o.time;
     }
 }
