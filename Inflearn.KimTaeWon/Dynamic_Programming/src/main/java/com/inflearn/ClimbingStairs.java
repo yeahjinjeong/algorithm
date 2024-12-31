c0d778a6bedd06be974a9072cb5c9c8c83ba70c4
yeahjinjeong <jjinnyzzang@gmail.com>
2024-12-31 19:48:38 +0900

    Update: 계단 오르기 - 다이나믹 풀이(feat.피보나치) #11

--- Inflearn.KimTaeWon/Dynamic_Programming/src/main/java/com/inflearn/ClimbingStairs.java
+++ Inflearn.KimTaeWon/Dynamic_Programming/src/main/java/com/inflearn/ClimbingStairs.java
@@ -13,6 +13,30 @@ public class ClimbingStairs {
         climbingStairs.DFS(0);
 
         System.out.println(answer);
+        System.out.println(climbingStairs.solution());
+    }
+
+    /**
+     * 1번째 계단까지 오는 경우의 수 1
+     * 2번째 계단으로 오는 경우의 수 2 = 바닥에서 오거나, 1번째 계단에서 오거나
+     * 3번째 계단으로 오는 경우의 수 3 = 1번째 계단에서 오거나, 2번째 계단에서 오거나
+     * ...
+     * n번째 계단으로 오는 경우의 수 = n-2번째 계단에서 오거나, n-1번째 계단에서 오거나
+     * 따라서 n-2번째 경우의 수 + n-1번째 경우의 수 = n번째 경우의 수
+     * => 피보나치 수열
+     */
+    
+    private int solution() {
+        int[] dy = new int[N + 1];
+
+        dy[1] = 1;
+        dy[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
+
+        return dy[N];
     }
 
     private void DFS(int i) {
@@ -24,6 +48,4 @@ public class ClimbingStairs {
             DFS(i + 2);
         }
     }
-
-    
 }
