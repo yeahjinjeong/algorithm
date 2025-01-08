31eea1edb14c6a05f9103eb1fac78507423eb2ad
yeahjinjeong <jjinnyzzang@gmail.com>
2025-01-08 17:18:39 +0900

    Update: 단지번호 붙이기 속도 개선(feat. queue 출력시 poll()로 꺼내기!) 17688KB 184ms #13

--- BaekJoon/DFSBFS/src/main/java/com/baekjoon/_2667.java
+++ BaekJoon/DFSBFS/src/main/java/com/baekjoon/_2667.java
@@ -6,21 +6,21 @@ public class _2667 {
     static int n;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
-    static int area = 0;
-    static int[][] board, visit;
+    static boolean[][] board;
+    static int[][] visit;
     public static void main(String[] args) {
         _2667 _2667 = new _2667();
 
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
-        board = new int[n][n];
+        board = new boolean[n][n];
         visit = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             String s = sc.next();
             for (int j = 0; j < n; j++) {
-                board[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+                board[i][j] = s.charAt(j) == '1';
             }
         }
 
@@ -28,40 +28,37 @@ public class _2667 {
     }
 
     private void DFSSolution() {
+        PriorityQueue<Integer> answer = new PriorityQueue<>();
 
         int num = 1;
-        List<Integer> answer = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) {
-                    board[i][j] = 0;
-                    visit[i][j] = num;
-                    DFS(i, j, num);
+                if (board[i][j] && visit[i][j] == 0) {
+                    answer.add(DFS(i, j, num));
                     num++;
-                    answer.add(area);
-                    area = 0;
                 }
             }
         }
 
-        System.out.println(answer.size());
-        Collections.sort(answer);
-        for (int a : answer) {
-            System.out.println(a);
+        System.out.println(num - 1);
+//        for (int a : answer) {
+//            System.out.println(a);
+//        } // 백준에서 이게 틀렸습니다로 나옴
+        while (!answer.isEmpty()) {
+            System.out.println(answer.poll());
         }
     }
 
-    private void DFS(int y, int x, int num) {
-        area++;
+    private int DFS(int y, int x, int num) {
+        visit[y][x] = num;
+        int area = 1;
         for (int i = 0; i < dx.length; i++) {
             int ny = y  + dy[i];
             int nx = x  + dx[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[ny][nx] == 1) {
-                board[ny][nx] = 0;
-                visit[ny][nx] = num;
-                DFS(ny, nx, num);
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[ny][nx] && visit[ny][nx] == 0) {
+                area += DFS(ny, nx, num);
             }
         }
+        return area;
     }
 }
