b63d066c27eaab8c4fd2908be699788da75e3d51
yeahjinjeong <jjinnyzzang@gmail.com>
2025-02-15 23:13:01 +0900

    Fix: Greedy-곱하기혹은더하기 (1의경우추가) #15

--- Python/Greedy/곱하기혹은더하기.py
+++ Python/Greedy/곱하기혹은더하기.py
@@ -1,14 +1,21 @@
 S = list(map(int, input()))
 
-result = 0
-flag = '+'
-for i in S:
-    if i != 0:
+result = S[0]
+if S[0] == 0 or S[0] == 1:
+    flag = '+'
+else:
+    flag = '*'
+
+for i in range(1, len(S)):
+    if S[i] > 1:
         if flag == '+':
-            result += i
+            result += S[i]
             flag = '*'
         else:
-            result *= i
+            result *= S[i]
+    elif S[i] == 1:
+        result += S[i]
+        flag = '*'
     else:
         flag = '+'
 
