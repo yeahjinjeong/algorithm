b92e023a255a8c2ea46e7f64866a3072167eb493
yeahjinjeong <jjinnyzzang@gmail.com>
2025-02-16 23:14:37 +0900

    Update 곱하기혹은더하기.py

--- Python/Greedy/곱하기혹은더하기.py
+++ Python/Greedy/곱하기혹은더하기.py
@@ -1,22 +1,30 @@
 S = list(map(int, input()))
 
 result = S[0]
-if S[0] == 0 or S[0] == 1:
-    flag = '+'
-else:
-    flag = '*'
+
+# if S[0] == 0 or S[0] == 1:
+#     flag = '+'
+# else:
+#     flag = '*'
+#
+# for i in range(1, len(S)):
+#     if S[i] > 1:
+#         if flag == '+':
+#             result += S[i]
+#             flag = '*'
+#         else:
+#             result *= S[i]
+#     elif S[i] == 1:
+#         result += S[i]
+#         flag = '*'
+#     else:
+#         flag = '+'
 
 for i in range(1, len(S)):
-    if S[i] > 1:
-        if flag == '+':
-            result += S[i]
-            flag = '*'
-        else:
-            result *= S[i]
-    elif S[i] == 1:
-        result += S[i]
-        flag = '*'
-    else:
-        flag = '+'
+    num = S[i]
+    if num <= 1 or result <= 1: # result가 0이나 1인 경우는 0, 1 혹은 0, ..., 1
+        result += num # 0이나 1이 앞이나 뒤에 있을 경우엔 앞에 수에 더한다
+    else :
+        result *= num # 0이나 1이 아니면 앞에 수에 곱한다
 
 print(result)
