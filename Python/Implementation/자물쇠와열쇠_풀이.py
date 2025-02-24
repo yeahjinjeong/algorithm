2d9fac5a5df36bb7c8131427fad7db339a4ef84b
yeahjinjeong <jjinnyzzang@gmail.com>
2025-02-24 16:12:51 +0900

    Update: 자물쇠와열쇠_이테코풀이 #15

---
+++ Python/Implementation/자물쇠와열쇠_풀이.py
@@ -0,0 +1,56 @@
+lock_size, key_size = map(int, input().split())
+lock = [[int(x) for x in input().split()] for _ in range(lock_size)]
+key = [[int(x) for x in input().split()] for _ in range(key_size)]
+
+"""
+3 3
+1 1 1
+1 1 0
+1 0 1
+0 0 0
+1 0 0
+0 1 1
+"""
+
+def rotate(a):
+    n = len(a)
+    m = len(a[0])
+    result = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
+
+
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+
+    for rotation in range(4):
+        key = rotate(key)
+        for x in range(n * 2):
+            for y in range(n * 2):
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                if check(new_lock) == True:
+                    return True
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+    return False
+
+print(solution(key, lock))
