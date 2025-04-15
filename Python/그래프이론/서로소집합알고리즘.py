131f10190d279fcab7610ef7cd548f69e655cb79
yeahjinjeong <jjinnyzzang@gmail.com>
2025-04-15 23:08:03 +0900

    Update 서로소집합알고리즘.py

--- Python/그래프이론/서로소집합알고리즘.py
+++ Python/그래프이론/서로소집합알고리즘.py
@@ -1,8 +1,10 @@
+# 특정 원소가 속한 집합을 찾기
 def find_parent(parent, x):
     if parent[x] != x:
         return find_parent(parent, parent[x])
     return x
 
+# 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
@@ -11,22 +13,27 @@ def union_parent(parent, a, b):
     else:
         parent[a] = b
 
+# 노드의 개수와 간선의 개수 입력받기
 v, e = map(int, input().split())
 parent = [0] * (v + 1)
 
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, v + 1):
     parent[i] = i
 
+# union 연산을 각각 수행
 for i in range(e):
     a, b = map(int, input().split())
     union_parent(parent, a, b)
 
+# 각 원소가 속한 집합 출력
 print('각 원소가 속한 집합: ', end = '')
 for i in range(1, v + 1):
     print(find_parent(parent, i), end = ' ')
 
 print()
 
+# 부모 테이블 내용 출력
 print('부모 테이블: ', end = '')
 for i in range(1, v + 1):
     print(parent[i], end = ' ')
